c5626147aeda691979414de3dfeecb8db805608e
Michal Gornicki <michal.gornicki@ithouse.co>
2023-02-07 13:03:34 +0100

    Updates

---
+++ precache-manifest.1f9bce4b11a39f17b6f11fcceea65523.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5444a433208ff61db73e050c9fb32147",
+    "url": "/game_vistas/index.html"
+  },
+  {
+    "revision": "593b328cce193e2f1125",
+    "url": "/game_vistas/static/css/main.5bc93b1b.chunk.css"
+  },
+  {
+    "revision": "9d3368280aeb91f90fa2",
+    "url": "/game_vistas/static/js/2.e1917bf5.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/game_vistas/static/js/2.e1917bf5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "593b328cce193e2f1125",
+    "url": "/game_vistas/static/js/main.2a412631.chunk.js"
+  },
+  {
+    "revision": "15bf78bca33abcb678ba",
+    "url": "/game_vistas/static/js/runtime-main.2c919f24.js"
+  }
+]);
